99b85cd11dc88ebb33601e451e29822c7a2fc045
Trey Warner <treywarner05@gmail.com>
2023-06-19 16:27:08 -0400

    Initatial init

---
+++ app.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+require 'sinatra/base'
+require 'logger'
+
+# App is the main application where all your logic & routing will go
+class App < Sinatra::Base
+  set :erb, escape_html: true
+  enable :sessions
+
+  attr_reader :logger, :groups, :typing_scores
+
+  def initialize
+    super
+    @logger = Logger.new('log/app.log')
+    @groups ||= begin
+      groups_from_id = `id`.to_s.match(/groups=(.+)/)[1].split(',').map do |g|
+        g.match(/\d+\((\w+)\)/)[1]
+      end
+
+      groups_from_id.select { |g| g.match?(/^P\w+/) }
+    end
+    
+  end
+
+  def title
+    'SwitftType'
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  get '/leaderboards' do
+    @typing_scores = File.foreach("#{__dir__}/typingscores").map { |line| line.split(' ')}.sort {|a,b| a[1].to_f*a[2].to_f <=> b[1].to_f*b[2].to_f}.reverse
+    
+    erb :leaderboards
+  end
+
+  get '/typing' do
+    @typing_scores = File.foreach("#{__dir__}/typingscores").map { |line| line.split(' ')}.sort {|a,b| a[1].to_f*a[2].to_f <=> b[1].to_f*b[2].to_f}.reverse
+    
+    file=File.open("#{__dir__}/wordlist")
+    words = file.read.tr('().\'",', '').split
+    len = 30
+    typing = Array.new(len) {words[ rand(0..words.size) ]}
+    @typing_text = typing.join(' ').downcase
+    erb :typing
+  end
+
+  post '/typing' do
+
+    name = params[:name].gsub(' ', '_')
+    wpm = params[:wpm]
+    acc = params[:acc]
+
+    session[:flash] = { info: "added new score for '#{name}' for #{wpm} and #{acc}" }
+    
+    open("#{__dir__}/typingscores", 'a') { |f|
+    f.print "\n#{name} #{wpm} #{acc}"
+    }
+    
+    redirect(url("/typing"))
+  end
+end
---
+++ public/typing.js
@@ -0,0 +1,110 @@
+
+
+    var idx=0
+    var count=0
+    var right=0
+    var countdown=0
+    var text_elem=document.getElementById("typing_text")
+    var text = text_elem.innerHTML
+    var highlight = []
+    
+    var wpm=document.getElementById("wpm")
+    var accuracy=document.getElementById("accuracy")
+
+    var start = null
+    var game =true
+    var rights=[]
+$(document.body).on('keydown', function(e) {
+    if(e.keyCode == 32) {
+        e.preventDefault();
+    }
+    if (game) {
+        if (!start) {
+            start=new Date();
+        }
+        char = String.fromCharCode((96 <= e.which && e.which <= 105)? e.which-48 : e.which).toLowerCase()
+        if (e.which == 8) {
+            if (idx>0) {
+                highlight[idx]=null
+                idx--
+                countdown++
+                if (rights[idx]==true)
+                    right--
+                highlight[idx]=null
+            }
+        } else {
+
+            if (text[idx] == char) {
+                rights[idx]=true
+                highlight[idx]='green'
+                //if (countdown>0) {
+                //    countdown--
+                //} else {
+                    right++
+                //}
+            } else {
+                rights[idx]=false
+                highlight[idx]='red'
+            }
+            
+            idx++
+        // if (countdown>0) {
+            //    countdown--
+            //    count+=2
+            //} else {
+            count++
+            //}
+        }
+
+        // change color
+        var new_text=""
+        for (var i=0; i<text.length; i++) {
+            if (highlight[i]) {
+                if (text[i]==' ' && highlight[i]=='red') {
+                    new_text=new_text+"<span style='background-color: "+highlight[i] + ";'>" + text[i] + "</span>"
+                
+                } else {
+                
+                    new_text=new_text+"<span style='color: "+highlight[i] + ";'>" + text[i] + "</span>"
+                }
+            } else {
+                new_text=new_text+text[i]
+            }
+        }
+        text_elem.innerHTML=new_text
+
+        //stats
+        now = new Date();
+        time = now-start;
+        wpm_i=Math.round((idx+1)/time*12000*100)/100
+        wpm.innerHTML = wpm_i
+        acc_i=Math.round((right)/idx*100)+"%"
+        accuracy.innerHTML = acc_i
+
+        //colors
+        if (wpm.innerHTML>100){
+            wpm.innerHTML = "<span style='color: green;'>" + wpm.innerHTML + "</span>"
+        } else if (wpm.innerHTML>75){
+            wpm.innerHTML = "<span style='color: gold;'>" + wpm.innerHTML + "</span>"
+        } else {
+            wpm.innerHTML = "<span style='color: red;'>" + wpm.innerHTML + "</span>"
+        }
+        
+        if (Number(accuracy.innerHTML.slice(0,-1))>95){
+            accuracy.innerHTML = "<span style='color: green;'>" + accuracy.innerHTML + "</span>"
+        } else if (Number(accuracy.innerHTML.slice(0,-1))>75){
+            accuracy.innerHTML = "<span style='color: gold;'>" + accuracy.innerHTML + "</span>"
+        } else {
+            accuracy.innerHTML = "<span style='color: red;'>" + accuracy.innerHTML + "</span>"
+        }
+    }
+    // end
+    if (idx==text.length) {
+        game = false
+        document.getElementById("score").innerHTML = "Congrats! You scored a WPM of "+wpm.innerHTML+" and an accuracy of "+accuracy.innerHTML+"!";
+        document.getElementById("score-submit").style.display = "block"
+        document.getElementById("info").style.display = "none"
+        document.getElementById("wpm-form").value = wpm_i
+        document.getElementById("acc-form").value = acc_i
+    }
+});
